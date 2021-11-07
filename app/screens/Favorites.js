52bb1af54b3c71507b8da6b55cc1796e93f3da50
Alberto <info@peralstudio.com>
2021-11-07 09:32:45 +0100

    Update Favorites.js

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -57,7 +57,7 @@ const Favorites = (props) => {
                     });
             }
             setReloadData(false);
-        }, [userLogged, reloadData, restaurants])
+        }, [userLogged, reloadData])
     );
 
     const getDataRestaurant = (idRestaurantsArray) => {
