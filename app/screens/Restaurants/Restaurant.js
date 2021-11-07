757ec732b8eac37d529072d238dbdc93b69592cb
Alberto <info@peralstudio.com>
2021-11-07 09:34:01 +0100

    Add acctions Restaurant

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
-import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    ScrollView,
+    Dimensions,
+    Linking,
+} from "react-native";
 import { map } from "lodash";
-import { Icon, ListItem, Rating } from 'react-native-elements';
-import { useFocusEffect } from '@react-navigation/native';
-import Toast from 'react-native-easy-toast';
+import { Icon, ListItem, Rating } from "react-native-elements";
+import { useFocusEffect } from "@react-navigation/native";
+import Toast from "react-native-easy-toast";
 import Loading from "../../components/Loading";
-import Carousel from "../../components/Carousel";
+import CarouselImage from "../../components/Carousel";
 import Map from "../../components/Map";
+import openMap from "react-native-open-maps";
 import Listreviews from "../../components/Restaurants/ListReviews";
 
 import { firebaseApp } from "../../utils/firebase";
@@ -14,7 +22,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const db = firebase.firestore(firebaseApp);
-const screenWidth = Dimensions.get('window').width;
+const screenWidth = Dimensions.get("window").width;
 
 const Restaurant = (props) => {
     const { navigation, route } = props;
@@ -23,6 +31,7 @@ const Restaurant = (props) => {
     const [rating, setRating] = useState(0);
     const [isFavorite, setIsFavorite] = useState(false);
     const [userLogged, setUserLogged] = useState(false);
+    const [activeSlide, setActiveSlide] = useState(0);
     const toastRef = useRef();
 
     navigation.setOptions({
@@ -32,7 +41,7 @@ const Restaurant = (props) => {
 
     firebase.auth().onAuthStateChanged((user) => {
         user ? setUserLogged(true) : setUserLogged(false);
-    })
+    });
 
     useFocusEffect(
         useCallback(() => {
@@ -50,60 +59,67 @@ const Restaurant = (props) => {
 
     useEffect(() => {
         if (userLogged && restaurant) {
-            db.collection('favorites')
-                .where('idRestaurant', '==', restaurant.id)
-                .where('idUser', '==', firebase.auth().currentUser.uid)
+            db.collection("favorites")
+                .where("idRestaurant", "==", restaurant.id)
+                .where("idUser", "==", firebase.auth().currentUser.uid)
                 .get()
                 .then((response) => {
                     if (response.docs.length === 1) {
                         setIsFavorite(true);
                     }
-                })
+                });
         }
-    }, [userLogged, restaurant])
+    }, [userLogged, restaurant]);
 
     const addFavorite = () => {
         if (!userLogged) {
-            toastRef.current.show('Debes estar registrado para añadir a favoritos este restaurante');
+            toastRef.current.show(
+                "Debes estar registrado para añadir a favoritos este restaurante"
+            );
         } else {
             const payload = {
                 idUser: firebase.auth().currentUser.uid,
-                idRestaurant: restaurant.id
-            }
-            db.collection('favorites')
+                idRestaurant: restaurant.id,
+            };
+            db.collection("favorites")
                 .add(payload)
                 .then(() => {
                     setIsFavorite(true);
-                    toastRef.current.show('Restaurante añadido a favoritos');
+                    toastRef.current.show("Restaurante añadido a favoritos");
                 })
                 .catch(() => {
-                    toastRef.current.show('Error al añadir el restaurante a favoritos');
-                })
+                    toastRef.current.show(
+                        "Error al añadir el restaurante a favoritos"
+                    );
+                });
         }
-    }
+    };
 
     const removeFavorite = () => {
-        db.collection('favorites')
-            .where('idRestaurant', '==', restaurant.id)
-            .where('idUser', '==', firebase.auth().currentUser.uid)
+        db.collection("favorites")
+            .where("idRestaurant", "==", restaurant.id)
+            .where("idUser", "==", firebase.auth().currentUser.uid)
             .get()
             .then((response) => {
                 response.forEach((doc) => {
                     const idfavorite = doc.id;
-                    db.collection('favorites')
+                    db.collection("favorites")
                         .doc(idfavorite)
                         .delete()
                         .then(() => {
                             setIsFavorite(false);
-                            toastRef.current.show('Restaurante eliminado de favoritos');
+                            toastRef.current.show(
+                                "Restaurante eliminado de favoritos"
+                            );
                         })
                         .catch(() => {
-                            toastRef.current.show('Error al eliminar restaurante de favoritos')
-                        })
-
-                })
-            })
-    }
+                            toastRef.current.show(
+                                "Error al eliminar restaurante de favoritos"
+                            );
+                        });
+                });
+            });
+    };
 
     if (!restaurant) return <Loading isVisible={true} text="Cargando..." />;
 
@@ -111,18 +127,20 @@ const Restaurant = (props) => {
         <ScrollView vertical style={styles.viewBody}>
             <View style={styles.viewFavorite}>
                 <Icon
-                    type='material-community'
-                    name={isFavorite ? 'heart' : 'heart-outline'}
+                    type="material-community"
+                    name={isFavorite ? "heart" : "heart-outline"}
                     onPress={isFavorite ? removeFavorite : addFavorite}
-                    color={isFavorite ? '#f00' : '#000'}
+                    color={isFavorite ? "#f00" : "#000"}
                     size={35}
-                    underlayColor='transparent'
+                    underlayColor="transparent"
                 />
             </View>
-            <Carousel
+            <CarouselImage
                 arrayImages={restaurant.images}
                 height={200}
                 width={screenWidth}
+                activeSlide={activeSlide}
+                setActiveSlide={setActiveSlide}
             />
             <TitleRestaurant
                 name={restaurant.name}
@@ -134,12 +152,11 @@ const Restaurant = (props) => {
                 location={restaurant.location}
                 name={restaurant.name}
                 address={restaurant.address}
+                phone={restaurant.phone}
+                email={restaurant.email}
             />
-            <Listreviews
-                navigation={navigation}
-                idRestaurant={restaurant.id}
-            />
-            <Toast ref={toastRef} position='center' opacity={0.9} />
+            <Listreviews navigation={navigation} idRestaurant={restaurant.id} />
+            <Toast ref={toastRef} position="center" opacity={0.9} />
         </ScrollView>
     );
 };
@@ -149,7 +166,7 @@ const TitleRestaurant = (props) => {
 
     return (
         <View style={styles.viewRestaurantTitle}>
-            <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexDirection: "row" }}>
                 <Text style={styles.nameRestaurant}>{name}</Text>
                 <Rating
                     style={styles.rating}
@@ -157,36 +174,52 @@ const TitleRestaurant = (props) => {
                     readonly
                     startingValue={parseFloat(rating)}
                 />
-                <View style={{ justifyContent: 'center', }} >
-                    <Text style={{ marginLeft: 5, fontSize: 16 }}>({quantityVoting})</Text>
+                <View style={{ justifyContent: "center" }}>
+                    <Text style={{ marginLeft: 5, fontSize: 16 }}>
+                        ({quantityVoting})
+                    </Text>
                 </View>
             </View>
             <Text style={styles.descriptionRestaurant}>{description}</Text>
         </View>
-    )
-}
+    );
+};
+
+const phoneCall = (phone) => Linking.openURL(`tel:${phone}`);
+
+const sendEmail = (to) => Linking.openURL(`mailto:${to}`);
+
+const openAppMap = (location, address, name) => {
+    openMap({
+        latitude: location.latitude,
+        longitude: location.longitude,
+        zoom: 18,
+        end: address,
+        query: name,
+    });
+};
 
 const RestaurantInfo = (props) => {
-    const { location, name, address } = props;
+    const { location, name, address, phone, email } = props;
 
     const listInfo = [
         {
             text: address,
             iconName: "map-marker",
             iconType: "material-community",
-            action: null,
+            action: () => openAppMap(location, address, name),
         },
         {
-            text: "111 222 333",
-            iconName: "phone",
+            text: phone,
+            iconName: "phone-in-talk",
             iconType: "material-community",
-            action: null,
+            action: () => phoneCall(phone),
         },
         {
-            text: "info@peralstudio.com",
-            iconName: "at",
+            text: email,
+            iconName: "email-plus",
             iconType: "material-community",
-            action: null,
+            action: () => sendEmail(email),
         },
     ];
 
@@ -195,48 +228,61 @@ const RestaurantInfo = (props) => {
             <Text style={styles.restaurantInfoTitle}>
                 Información sobre el restaurante
             </Text>
-            <Map
-                location={location}
-                name={name}
-                height={100}
-            />
+            <Map location={location} name={name} address={address} height={100} />
             {map(listInfo, (item, index) => (
-                <ListItem key={index} bottomDivider style={{ textAlignVertical: 'center' }}>
-                    <Icon name={item.iconName} type={item.iconType} color='#00a680' />
-                    <ListItem.Content >
-                        <ListItem.Title>{item.text}</ListItem.Title>
+                <ListItem
+                    key={index}
+                    bottomDivider
+                    style={{ textAlignVertical: "center" }}
+                    onPress={item.action}
+                >
+                    <Icon
+                        name={item.iconName}
+                        type={item.iconType}
+                        color="#00a680"
+                    />
+                    <ListItem.Content>
+                        <ListItem.Title style={{ width: "100%" }}>
+                            {item.text}
+                        </ListItem.Title>
                     </ListItem.Content>
-                    <ListItem.Chevron />
+                    {/* {phone || email ? (
+                        <View style={styles.viewAction}>
+                            <Icon
+                                name={item.iconNameRight}
+                                type={item.iconType}
+                                color="#00a680"
+                                onPress={item.action}
+                            />
+                        </View>
+                    ) : null} */}
                 </ListItem>
             ))}
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     viewBody: {
         flex: 1,
-        backgroundColor: '#fff'
+        backgroundColor: "#fff",
     },
     viewRestaurantTitle: {
         padding: 15,
     },
     nameRestaurant: {
         fontSize: 20,
-        fontWeight: 'bold',
-        width: 190
+        fontWeight: "bold",
+        width: 190,
     },
     descriptionRestaurant: {
         marginTop: 5,
-        color: 'grey',
+        color: "grey",
     },
     rating: {
-        // position: 'absolute',
-        // right: 20,
-        // top: -10,
         flex: 1,
-        alignItems: 'flex-end',
-        justifyContent: 'center',
+        alignItems: "flex-end",
+        justifyContent: "center",
     },
     viewRestaurantInfo: {
         margin: 15,
@@ -244,23 +290,27 @@ const styles = StyleSheet.create({
     },
     restaurantInfoTitle: {
         fontSize: 20,
-        fontWeight: 'bold',
+        fontWeight: "bold",
         marginBottom: 10,
     },
     containerListItem: {
-        borderBottomColor: '#d8d8d8',
+        borderBottomColor: "#d8d8d8",
         borderBottomWidth: 2,
     },
     viewFavorite: {
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         right: 0,
         zIndex: 2,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderBottomLeftRadius: 100,
         paddingLeft: 15,
-        padding: 5
-    }
+        padding: 5,
+    },
+    viewAction: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
 });
 
 export default Restaurant;
