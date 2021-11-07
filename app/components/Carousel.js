a85e9028f97b5167df28bf2025724280ce34a9bc
Alberto <info@peralstudio.com>
2021-11-07 09:30:16 +0100

    Add Carousel Dots

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -1,28 +1,74 @@
-import React from 'react';
-import { Image } from 'react-native-elements'
-import Carousel from 'react-native-snap-carousel'
+import { size } from "lodash";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Image } from "react-native-elements";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 
 const CarouselImage = (props) => {
-
-    const { arrayImages, height, width } = props;
+    const { arrayImages, height, width, activeSlide, setActiveSlide } = props;
 
     const renderItem = ({ item }) => {
-        return <Image
-            style={{ width, height }}
-            source={{ uri: item }}
-        />
-    }
+        return <Image style={{ width, height }} source={{ uri: item }} />;
+    };
 
     return (
-        <Carousel
-            layout={'default'}
-            data={arrayImages}
-            sliderWidth={width}
-            itemWidth={width}
-            renderItem={renderItem}
+        <View>
+            <Carousel
+                layout={"default"}
+                data={arrayImages}
+                sliderWidth={width}
+                itemWidth={width}
+                renderItem={renderItem}
+                onSnapToItem={(index) => {
+                    setActiveSlide(index);
+                }}
+            />
+            <MyPagination
+                arrayImages={arrayImages}
+                activeSlide={activeSlide}
+            />
+        </View>
+    );
+};
+
+const MyPagination = (props) => {
+    const { arrayImages, activeSlide } = props;
+
+    return (
+        <Pagination
+            dotsLength={size(arrayImages)}
+            activeDotIndex={activeSlide}
+            containerStyle={styles.paginationContainer}
+            dotStyle={styles.paginationDotActive}
+            inactiveDotStyle={styles.paginationDotInactive}
+            inactiveDotOpacity={0.9}
+            inactiveDotScale={0.6}
         />
     );
-}
+};
 
+const styles = StyleSheet.create({
+    paginationContainer: {
+        backgroundColor: "transparent",
+        zIndex: 1,
+        position: "absolute",
+        bottom: -20,
+        alignSelf: "center",
+    },
+    paginationDotActive: {
+        width: 10,
+        height: 10,
+        borderRadius: 10,
+        marginHorizontal: 5,
+        backgroundColor: "#00a680",
+    },
+    paginationDotInactive: {
+        width: 14,
+        height: 14,
+        borderRadius: 7,
+        marginHorizontal: 5,
+        backgroundColor: "white",
+    },
+});
 
 export default CarouselImage;
